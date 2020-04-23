4a1aa8e865eb1c56a53dbda285cdc851999b798f
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-04-23 15:58:55 -0400

    added an add and remove command as well as a prefix changer

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-import os, discord, asyncio
+import os, discord, asyncio, sqlite3
+from discord.ext import commands
 from dotenv import load_dotenv
-
+from utilities import database
 load_dotenv()
 
 class chh_bot(discord.Client):
@@ -11,27 +12,72 @@ class chh_bot(discord.Client):
     async def on_message(self, message):
         yes = "\U0001F4AF"
         no = "\U0001F6AB"
-        ids = [702929735038271598, 685566940122447887, 365879579887534082]
+        server_id = message.guild.id
+        if not os.path.exists("chh.db"):
+            database.create_table()
+            database.add_server(server_id, "&")
+        cmd_prefix = database.get_prefix(server_id)
+        channel_ids = database.get_allowed_channels()
+        suggestion_prefixs = ["[SUBREDDIT]","[DISCORD]","[CHH]"]
+        is_mod = False
+        for r in message.author.roles:
+            for p in r.permissions:
+                if p[0] == "administrator" and p[1] == True:
+                    is_mod = True
         if message.author == self.user:
             return
-        elif message.channel.id in ids:
-            if (message.content.startswith("[SUBREDDIT]") or
-                message.content.startswith("[DISCORD]") or
-                message.content.startswith("[CHH]")):
-                await message.add_reaction(yes)
-                await message.add_reaction(no)
-            else:
-                for r in message.author.roles:
-                    if not r.id == "613467520640221208":
-                        await message.delete()
-                        temp_message = await message.channel.send('%s please use [SUBREDDIT], [DISCORD] or [CHH] for your suggestions' % message.author.mention)
-                        await asyncio.sleep(5)
+        elif message.channel.id in channel_ids:
+            valid_msg = False
+            for prefix in suggestion_prefixs:
+                if message.content.startswith(prefix):
+                    await message.add_reaction(yes)
+                    await message.add_reaction(no)
+                    valid_msg = True
+            if not valid_msg:
+                if not is_mod:
+                    send_channel = message.channel
+                    await message.delete()
+                    temp_message = await send_channel.send('%s please use [SUBREDDIT], [DISCORD] or [CHH] for your suggestions' % message.author.mention)
+                    await asyncio.sleep(5)
+                    await temp_message.delete()
+                if is_mod:
+                    if message.content.startswith("{}prefix".format(cmd_prefix)):
+                        new_prefix = message.content.split()[1]
+                        if len(new_prefix) > 2:
+                            temp_message = await message.channel.send('Prefix should be under two characters long')
+                            await asyncio.sleep(3)
+                            await temp_message.delete()
+                        elif new_prefix == "":
+                            temp_message = await message.channel.send('Please include a prefix')
+                            await asyncio.sleep(3)
+                            await temp_message.delete()
+                        else:
+                            database.set_prefix(server_id, new_prefix)
+                    elif message.content.startswith("{}remove".format(cmd_prefix)):
+                        database.remove_channel(message.channel.id)
+                        temp_message = await message.channel.send('removed this channel from monitored channels')
+                        await asyncio.sleep(3)
+                        await temp_message.delete()
+
+        elif is_mod:
+            if message.content.startswith("{}add".format(cmd_prefix)):
+                database.add_channels(message.channel.id)
+                temp_message = await message.channel.send('Added this channel to monitored channels')
+                await asyncio.sleep(3)
+                await temp_message.delete()
+            elif message.content.startswith("{}prefix".format(cmd_prefix)):
+                if len(message.content.split()) == 1:
+                    temp_message = await message.channel.send('Please include a prefix')
+                    await asyncio.sleep(3)
+                    await temp_message.delete()
+                else:
+                    new_prefix = message.content.split()[1]
+                    if len(new_prefix) > 2:
+                        temp_message = await message.channel.send('Prefix should be under two characters long')
+                        await asyncio.sleep(3)
                         await temp_message.delete()
                     else:
-                        if message.content.startswith("^^clear"):
-                            msgs = []
-                            async for x in self.logs_from(message.channel, limit=number):
-                                msgs.append(x)
+                        database.set_prefix(server_id, new_prefix)
 
 
 
