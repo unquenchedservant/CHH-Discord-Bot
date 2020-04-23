ba2ba9d5b9567c36ade615da2e1c1890b4fa6fe2
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-04-23 14:15:38 -0400

    Bot so far can respond to messages in certain channels

---
+++ bot.py
@@ -0,0 +1,44 @@
+import os, discord, asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class chh_bot(discord.Client):
+
+    async def on_ready(self):
+        print("Logged on as {0}!".format(self.user))
+
+    async def on_message(self, message):
+        yes = "\U0001F4AF"
+        no = "\U0001F6AB"
+        if message.author == self.user:
+            return
+        if (message.channel.id == '702929735038271598' or
+            message.channel.id == '685566940122447887' or
+            message.channel.id == '365879579887534082'):
+            await message.channel.send("TESTING")
+            if (message.content.startswith("[SUBREDDIT]") or
+                message.content.startswith("[DISCORD]") or
+                message.content.startswith("[CHH]")):
+                await message.add_reaction(yes)
+                await message.add_reaction(no)
+            else:
+                for r in message.author.roles:
+                    if not r.id == "613467520640221208":
+                        await message.delete()
+                        temp_message = await message.channel.send('%s please use [SUBREDDIT], [DISCORD] or [CHH] for your suggestions' % message.author.mention)
+                        await asyncio.sleep(5)
+                        await temp_message.delete()
+                    else:
+                        if message.content.startswith("^^clear"):
+                            msgs = []
+                            async for x in self.logs_from(message.channel, limit=number):
+                                msgs.append(x)
+
+
+
+
+
+client = chh_bot()
+TOKEN=os.getenv('DISCORD_TOKEN')
+client.run(TOKEN)
