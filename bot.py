025075bc24c7b3b654f5a3eed913984b711ceb9d
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-04-23 22:30:21 -0400

    hopefully fixed db problem

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ load_dotenv()
 class chh_bot(discord.Client):
 
     async def on_ready(self):
+        database.create_table()
         print("Logged on as {0}!".format(self.user))
 
     async def on_raw_reaction_add(self, payload):
@@ -44,6 +45,7 @@ class chh_bot(discord.Client):
 
 
         server_id = message.guild.id
+        database.add_server(server_id, "^")
 
         if not os.path.exists("chh.db"):
             database.create_table()
@@ -51,14 +53,9 @@ class chh_bot(discord.Client):
 
         cmd_prefix = database.get_prefix(server_id)
 
-        if cmd_prefix == "&":
-            database.set_prefix(server_id, "^")
-            cmd_prefix = "^"
-
         channel_ids = database.get_allowed_channels()
-        print(channel_ids)
         recommended_channel_ids = database.get_allowed_recommended_channels()
-        print(recommended_channel_ids)
+
         suggestion_prefixs = ["[SUBREDDIT]","[DISCORD]","[CHH]"]
 
         is_mod = False
@@ -71,104 +68,105 @@ class chh_bot(discord.Client):
         if message.author == self.user:
             return
         # START MOD COMMANDS
-        if is_mod:
-            # ADD CHANNEL TO MONITORING
-            if message.content.startswith("{}add".format(cmd_prefix)):
-                directive = message.content.replace("{}add".format(cmd_prefix), "")
-                directive = directive.replace(" ", "")
-                if directive  == "recommended":
-                    database.add_recommended_channel(message.channel.id)
-                    temp_message = await message.channel.send('Added this channel to monitored channels')
-                    await message.delete()
-                    await asyncio.sleep(3)
-                    await temp_message.delete()
-                elif directive == "suggestion":
-                    database.add_channels(message.channel.id)
-                    temp_message = await message.channel.send('Added this channel to monitored channels')
-                    await message.delete()
-                    await asyncio.sleep(3)
-                    await temp_message.delete()
-                else:
-                    await message.channel.send("Unknown command, use '{0}add recommended' or '{0}add suggestion' ".format(cmd_prefix))
-
-
-            # REMOVE CHANNEL FROM MONITORING
-            elif message.content.startswith("{}remove".format(cmd_prefix)):
-                directive = message.content.replace("{}remove".format(cmd_prefix), "")
-                directive = directive.replace(" ", "")
-                if directive  == "recommended":
-                    database.remove_recommended_channel(message.channel.id)
-                    temp_message = await message.channel.send('No longer monitoring this channel')
-                    await message.delete()
-                    await asyncio.sleep(3)
-                    await temp_message.delete()
-                elif directive == "suggestion":
-                    database.remove_channel(message.channel.id)
-                    temp_message = await message.channel.send('No longer monitoring this channel')
-                    await message.delete()
-                    await asyncio.sleep(3)
-                    await temp_message.delete()
-                else:
-                    await message.channel.send("Unknown command, use '{0}remove recommended' or '{0}remove suggestion' ".format(cmd_prefix))
-
-            # CHANGE THE PREFIX
-            elif message.content.startswith("{}prefix".format(cmd_prefix)):
-                if len(message.content.split()) == 1:
-                    temp_message = await message.channel.send('Please include a prefix')
-                    await asyncio.sleep(3)
-                    await temp_message.delete()
-                else:
-                    new_prefix = message.content.split()[1]
-                    if len(new_prefix) > 2:
-                        temp_message = await message.channel.send('Prefix should be under two characters long')
+        else:
+            if is_mod:
+                # ADD CHANNEL TO MONITORING
+                if message.content.startswith("{}add".format(cmd_prefix)):
+                    directive = message.content.replace("{}add".format(cmd_prefix), "")
+                    directive = directive.replace(" ", "")
+                    if directive  == "recommended":
+                        database.add_recommended_channel(message.channel.id)
+                        temp_message = await message.channel.send('Added this channel to monitored channels')
+                        await message.delete()
+                        await asyncio.sleep(3)
+                        await temp_message.delete()
+                    elif directive == "suggestion":
+                        database.add_channels(message.channel.id)
+                        temp_message = await message.channel.send('Added this channel to monitored channels')
+                        await message.delete()
                         await asyncio.sleep(3)
                         await temp_message.delete()
                     else:
-                        database.set_prefix(server_id, new_prefix)
+                        await message.channel.send("Unknown command, use '{0}add recommended' or '{0}add suggestion' ".format(cmd_prefix))
 
-            # CLEAR THE CHAT
-            elif message.content.startswith("{}clear".format(cmd_prefix)):
-                msgs = []
-                if len(message.content.split()) > 1:
-                    number = int(message.content.split()[1]) + 1
-                else:
-                    number = 6
-                await message.channel.purge(limit = number)
 
-        # IF USER NOT MOD OR IF CHANNEL ID IS IN MONITORED LIST
-        if not message.content.startswith(cmd_prefix):
-            if message.channel.id in channel_ids:
-                valid_msg = False
-
-                # CHECK IF USING SUGGESTION PREFIX
-                for prefix in suggestion_prefixs:
-                    if message.content.startswith(prefix):
-                        await message.add_reaction(yes)
-                        await message.add_reaction(no)
-                        valid_msg = True
-
-                # IF NOT USING PREFIX
-                if not valid_msg:
-                    if not is_mod:
-                        send_channel = message.channel
-                        await message.delete() # DELETE THEIR MESSAGE
-                        temp_message = await send_channel.send('%s please use [SUBREDDIT], [DISCORD] or [CHH] for your suggestions' % message.author.mention)
-                        await asyncio.sleep(5)
+                # REMOVE CHANNEL FROM MONITORING
+                elif message.content.startswith("{}remove".format(cmd_prefix)):
+                    directive = message.content.replace("{}remove".format(cmd_prefix), "")
+                    directive = directive.replace(" ", "")
+                    if directive  == "recommended":
+                        database.remove_recommended_channel(message.channel.id)
+                        temp_message = await message.channel.send('No longer monitoring this channel')
+                        await message.delete()
+                        await asyncio.sleep(3)
                         await temp_message.delete()
+                    elif directive == "suggestion":
+                        database.remove_channel(message.channel.id)
+                        temp_message = await message.channel.send('No longer monitoring this channel')
+                        await message.delete()
+                        await asyncio.sleep(3)
+                        await temp_message.delete()
+                    else:
+                        await message.channel.send("Unknown command, use '{0}remove recommended' or '{0}remove suggestion' ".format(cmd_prefix))
 
-        if message.channel.id in recommended_channel_ids:
-            if message.content.startswith("{}recommend".format(cmd_prefix)):
-                search_string = message.content.replace("{}recommend".format(cmd_prefix), "")
-                search_string = search_string.replace(" ", "")
-                if search_string == "":
-                    await message.channel.send("%s Please specify an artist name to recommend")
-                else:
-                    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-                    results = sp.search(q=search_string, limit=1, type='artist')
-                    items = results['artists']['items']
-                    if len(items) > 0:
-                        artist = items[0]
-                        await message.channel.send(artist['external_urls']['spotify'])
+                # CHANGE THE PREFIX
+                elif message.content.startswith("{}prefix".format(cmd_prefix)):
+                    if len(message.content.split()) == 1:
+                        temp_message = await message.channel.send('Please include a prefix')
+                        await asyncio.sleep(3)
+                        await temp_message.delete()
+                    else:
+                        new_prefix = message.content.split()[1]
+                        if len(new_prefix) > 2:
+                            temp_message = await message.channel.send('Prefix should be under two characters long')
+                            await asyncio.sleep(3)
+                            await temp_message.delete()
+                        else:
+                            database.set_prefix(server_id, new_prefix)
+
+                # CLEAR THE CHAT
+                elif message.content.startswith("{}clear".format(cmd_prefix)):
+                    msgs = []
+                    if len(message.content.split()) > 1:
+                        number = int(message.content.split()[1]) + 1
+                    else:
+                        number = 6
+                    await message.channel.purge(limit = number)
+
+            # IF USER NOT MOD OR IF CHANNEL ID IS IN MONITORED LIST
+            if not message.content.startswith(cmd_prefix):
+                if message.channel.id in channel_ids:
+                    valid_msg = False
+
+                    # CHECK IF USING SUGGESTION PREFIX
+                    for prefix in suggestion_prefixs:
+                        if message.content.startswith(prefix):
+                            await message.add_reaction(yes)
+                            await message.add_reaction(no)
+                            valid_msg = True
+
+                    # IF NOT USING PREFIX
+                    if not valid_msg:
+                        if not is_mod:
+                            send_channel = message.channel
+                            await message.delete() # DELETE THEIR MESSAGE
+                            temp_message = await send_channel.send('%s please use [SUBREDDIT], [DISCORD] or [CHH] for your suggestions' % message.author.mention)
+                            await asyncio.sleep(5)
+                            await temp_message.delete()
+            elif message.content.startswith(cmd_prefix):
+                if message.channel.id in recommended_channel_ids:
+                    if message.content.startswith("{}recommend".format(cmd_prefix)):
+                        search_string = message.content.replace("{}recommend".format(cmd_prefix), "")
+                        search_string = search_string.replace(" ", "")
+                        if search_string == "":
+                            await message.channel.send("%s Please specify an artist name to recommend" % message.author.mention)
+                        else:
+                            sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+                            results = sp.search(q=search_string, limit=1, type='artist')
+                            items = results['artists']['items']
+                            if len(items) > 0:
+                                artist = items[0]
+                                await message.channel.send(artist['external_urls']['spotify'])
 
 
 
