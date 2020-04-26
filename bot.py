161e87593b55551f3b0698e03e2bdacbc68a84b1
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-04-26 13:12:29 -0400

    moved all the admin commands to a cog

--- bot.py
+++ bot.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 import os, discord, asyncio, sqlite3, logging, spotipy
 from discord.ext import commands
-from discord.ext.commands import has_permissions, CheckFailure
+from utilities import database, get_env
 from discord.utils import get
 from dotenv import load_dotenv
 from utilities import database
 from spotipy.oauth2 import SpotifyClientCredentials
 
-load_dotenv()
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename="chh_bot.log", encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-
 
 default_prefix = "^"
 async def determine_prefix(bot, message):
@@ -26,15 +24,10 @@ async def determine_prefix(bot, message):
 description = '''This bot monitors the suggestion channel and also adds some process_commands that are specific to the r/chh discord server'''
 bot = commands.Bot(command_prefix=determine_prefix, description=description)
 
-def check_is_mod(user):
-    is_mod = False
-    for r in user.roles:
-        for p in r.permissions:
-            if p[0] == "administrator" and p[1] == True:
-                is_mod = True
-    return is_mod
+if __name__ == '__main__':
+    for extension in extensions:
+        bot.load_extension(extension)
 
-@bot.event
 async def on_reaction_add(reaction, user):
     if not user.id == bot.user.id:
         message_id = reaction.message.id
@@ -141,95 +134,10 @@ async def on_message(message):
 
     else:
         await bot.process_commands(message)
-
-@bot.command(hidden=True, usage="<number of messages to purge>", description="Clear the previous X messages on this channel")
-@has_permissions(administrator=True)
-async def clear(ctx, amount: int):
-    msgs = []
-    await ctx.channel.purge(limit=amount+1)
-
-@bot.command(hidden=True, aliases=["set_prefix"], usage="<wanted_prefix>", description="Set a max 2-character prefix for this bot on this server")
-@has_permissions(administrator=True)
-async def prefix(ctx, new_prefix=""):
-    if new_prefix == "":
-        embed = discord.Embed(title="Prefix", description="Get or change the server prefix", colour=0x0099ff)
-        embed.set_author(name="r/CHH Bot", icon_url="https://i.imgur.com/ZNdCFKg.png")
-        embed.add_field(name="Current prefix", value="{}".format(database.get_prefix(ctx.guild.id)))
-        embed.add_field(name="\u200B", value="\u200B")
-        embed.add_field(name="\u200B", value="\u200B")
-        embed.add_field(name="Change Prefix", value="To change the prefix, use {}prefix <new prefix>".format(database.get_prefix(ctx.guild.id)), inline=True)
-        await ctx.channel.send(embed=embed)
-    else:
-        if len(new_prefix) > 2:
-            embed = discord.Embed(title="Invalid Prefix", description="Please make sure that your prefix is at max two characters", colour=0x0099ff)
-            embed.set_author(name="r/CHH Bot", icon_url="https://i.imgur.com/ZNdCFKg.png")
-            embed.add_field(name="Change Prefix", value="To change the prefix, use {}prefix <new prefix>".format(database.get_prefix(ctx.guild.id)), inline=True)
-            await ctx.channel.send(embed=embed)
         else:
-            database.set_prefix(ctx.guild.id, new_prefix)
-            await ctx.channel.send("Successfully updated prefix for server")
 
-@bot.command(hidden=True, aliases=["add"], usage="<suggestion|recommendations>", brief="Add a tracked channel", description="Start tracking a channel for suggestions or recommendations")
-@has_permissions(administrator=True)
-async def track(ctx, track_type=""):
-
-    if track_type == "suggestions":
-        success = database.add_suggestion_channel(ctx.channel.id)
-        if success:
-            temp_message = await ctx.channel.send("Now tracking this channel for suggestions")
-        else:
-            temp_message = await ctx.channel.send("Already tracking this channel for suggestions")
-        await asyncio.sleep(3)
-        await ctx.message.delete()
-        await temp_message.delete()
-
-    elif track_type == "recommendations":
-        success = database.add_recommendation_channel(ctx.channel.id)
-        if success:
-            temp_message = await ctx.channel.send("Now tracking this channel for recommendations")
-        else:
-            temp_message = await ctx.channel.send("Already tracking this channel for suggestions")
-        await asyncio.sleep(3)
-        await ctx.message.delete()
-        await temp_message.delete()
-    else:
-        embed = discord.Embed(title="Error Adding Tracking", description="Please specify one of the following", colour=0x0099ff)
-        embed.set_author(name="r/CHH Bot", icon_url="https://i.imgur.com/ZNdCFKg.png")
-        embed.add_field(name="suggestions", value="tracks a suggestion channel")
-        embed.add_field(name="\u200b", value="\u200B")
-        embed.add_field(name="\u200b", value="\u200B")
-        embed.add_field(name="recommendations", value="tracks a recommendation channel")
-        await ctx.channel.send(embed=embed)
-
-@bot.command(hidden=True, usage="<suggestions|recommendations>", brief="Remove a tracked channel", description="Remove a channel from being tracked for suggestions/recommendations")
-@has_permissions(administrator=True)
-async def remove(ctx, track_type=""):
-    if track_type == "suggestions":
-        success = database.remove_suggestion_channel(ctx.channel.id)
-        if success:
-            temp_message = await ctx.channel.send("No longer tracking this channel for suggestions")
-        else:
-            temp_message = await ctx.channel.send("Was not tracking this channel for suggestions")
-        await asyncio.sleep(3)
-        await ctx.message.delete()
-        await temp_message.delete()
-    elif track_type == "recommendations":
-        success = database.remove_recommendation_channel(ctx.channel.id)
-        if success:
-            temp_message = await ctx.channel.send("No longer tracking this channel for recommendations")
-        else:
-            temp_message = await ctx.channel.send("Was not tracking this channel for recommendations")
-        await asyncio.sleep(3)
-        await ctx.message.delete()
-        await temp_message.delete()
-    else:
-        embed = discord.Embed(title="Error Removing Tracking", description="Please specify one of the following", colour=0x0099ff)
-        embed.set_author(name="r/CHH Bot", icon_url="https://i.imgur.com/ZNdCFKg.png")
-        embed.add_field(name="suggestions", value="removes tracking for a suggestion channel")
-        embed.add_field(name="\u200b", value="\u200B")
-        embed.add_field(name="\u200b", value="\u200B")
-        embed.add_field(name="recommendations", value="removes tracking for a recommendation channel")
-        await ctx.channel.send(embed=embed)
+token = get_env.discord_token()
+bot.run(token, bot=True, reconnect=True)
 
 """Recommend an artist
 
@@ -274,6 +182,3 @@ async def recommend(ctx, *args):
                 primary_msg = await ctx.channel.send(artist_url)
                 await primary_msg.add_reaction("\U000023ED")
                 database.add_reaction_message(primary_msg.id, second_msg.id, ctx.message.author.id, search_term)
-
-token = os.getenv('DISCORD_TOKEN')
-bot.run(token)
---
+++ cogs/admin.py
@@ -0,0 +1,138 @@
+import os, discord, asyncio
+from discord.ext import commands
+from discord.ext.commands import has_permissions, group
+from utilities import database, get_env
+class Admin(commands.Cog, command_attrs=dict(case_insensitive=True)):
+    def __init__(self, bot):
+        self.bot = bot
+
+    """
+    Track/Add/Start Command Group
+
+    Subcommands:
+        suggestions
+        recommendations
+    """
+    @group(case_insensitive=True, aliases=["add", "start"], usage="<suggestion|recommendations>", brief="Add a tracked channel", description="Start listening to a channel for suggestions or recommendations")
+    @has_permissions(administrator=True)
+    async def track(self, ctx):
+        if ctx.invoked_subcommand is None:
+            embed = discord.Embed(title="Error Adding Tracking", description="Please specify one of the following", colour=0x0099ff)
+            embed.set_author(name="r/CHH Bot", icon_url="https://i.imgur.com/ZNdCFKg.png")
+            embed.add_field(name="suggestions", value="tracks a suggestion channel")
+            embed.add_field(name="\u200b", value="\u200B")
+            embed.add_field(name="\u200b", value="\u200B")
+            embed.add_field(name="recommendations", value="tracks a recommendation channel")
+            await ctx.channel.send(embed=embed)
+
+    @track.command(aliases=["suggest", "sug", "suggestion", "s"], description="Start listening to this channel for suggestions")
+    async def suggestions(self, ctx):
+        success = database.add_suggestion_channel(ctx.channel.id)
+        if success:
+            temp_message = await ctx.channel.send("Started listening to this channel for suggestions")
+        else:
+            temp_message = await ctx.channel.send("Already listening to this channel for suggestions")
+        await asyncio.sleep(3)
+        await ctx.message.delete()
+        await temp_message.delete()
+
+    @track.command(aliases=["recommendation", "recommend", "rec", "r"], description="Start listening to this channel for recommendations")
+    async def recommendations(self, ctx):
+        success = database.add_recommendation_channel(ctx.channel.id)
+        if success:
+            temp_message = await ctx.channel.send("Started listening to this channel for recommendations")
+        else:
+            temp_message = await ctx.channel.send("Already listening to this channel for recommendations")
+        await asyncio.sleep(3)
+        await ctx.message.delete()
+        await temp_message.delete()
+
+    """
+    Remove/Disable/Untrack/Stop Command Group
+
+    Subcommands:
+    - suggestions
+    - recommendations
+    """
+    @group(case_insensitive=True, aliases=["disable", "untrack", "stop"], usage="<suggestions|recommendations>", brief="Remove a tracked channel", description="Stops listening for suggestions/recommendations on a channel")
+    @has_permissions(administrator=True)
+    async def remove(self, ctx):
+        if ctx.invoked_subcommand is None:
+            embed = discord.Embed(title="Error Stopping Listening", description="Please specify one of the following", colour=0x0099ff)
+            embed.set_author(name="r/CHH Bot", icon_url="https://i.imgur.com/ZNdCFKg.png")
+            embed.add_field(name="suggestions", value="Stop listening for suggestions on the channel")
+            embed.add_field(name="\u200b", value="\u200B")
+            embed.add_field(name="\u200b", value="\u200B")
+            embed.add_field(name="recommendations", value="Stop listening for recommendations on the channel")
+            await ctx.channel.send(embed=embed)
+
+    @remove.command(aliases=["suggest", "sug", "suggestion", "s"], description="Stop listening to this channel for suggestions")
+    async def suggestions(self, ctx):
+        success = database.remove_suggestion_channel(ctx.channel.id)
+        if success:
+            temp_message = await ctx.channel.send("No longer listening to this channel for suggestions")
+        else:
+            temp_message = await ctx.channel.send("Was not listening this channel for suggestions")
+        await asyncio.sleep(3)
+        await ctx.message.delete()
+        await temp_message.delete()
+
+    @remove.command(aliases=["recommendation", "recommend", "rec", "r"], description="Stop listening to this channel for recommendations")
+    async def recommendations(self, ctx):
+        success = database.remove_recommendation_channel(ctx.channel.id)
+        if success:
+            temp_message = await ctx.channel.send("No longer listening to this channel for recommendations")
+        else:
+            temp_message = await ctx.channel.send("Was not listening this channel for recommendations")
+        await asyncio.sleep(3)
+        await ctx.message.delete()
+        await temp_message.delete()
+
+    """
+    Other Commands
+
+    Shutdown - Owner only, shuts the bot down remotely
+    Clear - AKA purge, clears X messages from the channel
+    Prefix - set the prefix for the server
+    """
+    @commands.command(hidden=True)
+    async def shutdown(self, ctx):
+        if ctx.message.author.id == int(get_env.owner_id()):
+            try:
+                await ctx.channel.send("Shutting Down")
+                await self.bot.logout()
+            except:
+                print("EnvironmentError")
+                self.bot.clear()
+        else:
+            await ctx.channel.send("Hey wait a second, you aren't my owner")
+    
+    @commands.command(aliases=["purge"], usage="<number of messages to purge>", brief="clear X amount of messages in channel", description="Clear the previous X messages on this channel")
+    @has_permissions(administrator=True)
+    async def clear(self, ctx, amount: int):
+        msgs = []
+        await ctx.channel.purge(limit=amount+1)
+
+    @commands.command(aliases=["set_prefix"], usage="<wanted_prefix>", brief="Change the prefix for the server", description="Set a max 2-character prefix for this bot on this server")
+    @has_permissions(administrator=True)
+    async def prefix(self, ctx, new_prefix=""):
+        if new_prefix == "":
+            embed = discord.Embed(title="Prefix", description="Get or change the server prefix", colour=0x0099ff)
+            embed.set_author(name="r/CHH Bot", icon_url="https://i.imgur.com/ZNdCFKg.png")
+            embed.add_field(name="Current prefix", value="{}".format(database.get_prefix(ctx.guild.id)))
+            embed.add_field(name="\u200B", value="\u200B")
+            embed.add_field(name="\u200B", value="\u200B")
+            embed.add_field(name="Change Prefix", value="To change the prefix, use {}prefix <new prefix>".format(database.get_prefix(ctx.guild.id)), inline=True)
+            await ctx.channel.send(embed=embed)
+        else:
+            if len(new_prefix) > 2:
+                embed = discord.Embed(title="Invalid Prefix", description="Please make sure that your prefix is at max two characters", colour=0x0099ff)
+                embed.set_author(name="r/CHH Bot", icon_url="https://i.imgur.com/ZNdCFKg.png")
+                embed.add_field(name="Change Prefix", value="To change the prefix, use {}prefix <new prefix>".format(database.get_prefix(ctx.guild.id)), inline=True)
+                await ctx.channel.send(embed=embed)
+            else:
+                database.set_prefix(ctx.guild.id, new_prefix)
+                await ctx.channel.send("Successfully updated prefix for server")
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
