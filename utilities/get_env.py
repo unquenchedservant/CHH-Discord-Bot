ed6149f539923f6e78cc88dadb9992836ec30a51
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-08-19 10:18:20 -0400

    added dev token

--- utilities/get_env.py
+++ utilities/get_env.py
@@ -7,6 +7,9 @@ load_dotenv()
 def discord_token():
     return os.getenv('DISCORD_TOKEN')
 
+def discord_dev():
+    return os.getenv('DISCORD_DEV')
+
 def owner_id():
     return os.getenv('OWNER_ID')
 
