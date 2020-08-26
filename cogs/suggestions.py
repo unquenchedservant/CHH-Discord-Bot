9442559386bf3571e18c974c0d51ec3c1d24e6b7
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-08-26 10:41:13 -0400

    you crafty people

--- cogs/suggestions.py
+++ cogs/suggestions.py
@@ -80,7 +80,7 @@ class Suggestions(commands.Cog):
                     if len(check_stick) < 10:
                         check_array = [''.join(p) for p in permutations(check_stick)]
                         for item in check_array:
-                            result = re.match(r'^[s$S]\s*[Tt]\s*[1iI]\s*[Cc]\s*[kK]\W*$', item)
+                            result = re.match(r'^[s$S\*]\s*[\*Tt]\s*[\*1iIl]\s*[\*(Cc]\s*[\*kK]\W*$', item)
                             if result:
                                 stick_found = True
                                 break
