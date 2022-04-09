6fe6f4b9dc317ef15e672c29f4f0c67c93265367
Jonathan Thorne <jonathanthorne13@gmail.com>
2022-04-09 14:52:02 -0400

    Added the ability to remember roles

--- bot.py
+++ bot.py
@@ -3,9 +3,12 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
-from utilities import get_env
+from utilities import get_env, database
 
-bot = commands.Bot(command_prefix="^", description="A quick bot with random stuff for the CHH discord", case_insensitive=True)
+intents = discord.Intents.all()
+intents.members = True
+
+bot = commands.Bot(command_prefix="^", description="A quick bot with random stuff for the CHH discord", case_insensitive=True, intents=intents)
 
 
 
@@ -14,6 +17,20 @@ extensions=['cogs.admin', 'cogs.reports', 'cogs.aprilfools', 'cogs.events']
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+
+@bot.event
+async def on_member_join(member):
+    rolesToAdd = database.getRoles(member.id)
+    for roleID in rolesToAdd:
+        role = get(member.guild.roles, id=roleID)
+        await member.add_roles(role, atomic=True)
+    database.removeRoles(member.id)
+    
+@bot.event
+async def on_member_remove(member):
+    for role in member.roles:
+        if not role.name == "@everyone":
+            database.addRole(member.id, role.id)
 
 if __name__ == '__main__':
     for extension in extensions:
--- cogs/events.py
+++ cogs/events.py
@@ -4,9 +4,5 @@ class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        print(member.roles)
-
 def setup(bot):
     bot.add_cog(Events(bot))
