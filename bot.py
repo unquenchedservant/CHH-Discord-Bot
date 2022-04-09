87f5426d5ba148088c266ad46ab5e34db298d6bf
Jonathan Thorne <jonathanthorne13@gmail.com>
2022-04-09 13:40:38 -0400

    change to dev mode

--- bot.py
+++ bot.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     for extension in extensions:
         bot.load_extension(extension)
 
-token = get_env.discord_token()
+token = get_env.discord_dev()
 bot.run(token, bot=True, reconnect=True)
