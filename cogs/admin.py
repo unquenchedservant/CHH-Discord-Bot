e33707bd6d43be4ff0000dc4457e11e182a8ab77
Jonathan Thorne <jonathanthorne13@gmail.com>
2022-05-09 18:25:21 -0400

    ported admin cog to slash commands

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,36 +1,57 @@
-import os
 import discord
-import asyncio
+from discord.commands import (slash_command)
+from discord.commands import Option
 from discord.ext import commands
-from discord.ext.commands import has_permissions, group
 from utilities import database
 
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
     
-    @commands.command(pass_context=True, aliases=["setreport","addreport", "updatereport"])
-    @has_permissions(administrator=True)
-    async def setreportchannel(self, ctx):
-        guild = ctx.guild
-        channel = ctx.channel
-        if database.lookUpGuildReport(guild.id):
-            database.updateGuildReport(guild.id, channel.id)
-            await ctx.send("Updated the report channel for this server")
-        else:
-            database.setGuildReport(guild.id, channel.id)
-            await ctx.send("Added a report channel for this server")
-    @commands.command(pass_context=True, name="removereportchannel", aliases=["removeReport"])
-    @has_permissions(administrator=True)
-    async def removereportchannel(self, ctx):
+    @slash_command(guild_ids=[365879579887534080], default_permission=False)
+    @commands.has_any_role("ADMIN", "MOD")
+    async def setreportchannel(self, ctx: discord.ApplicationContext, channel: Option(discord.TextChannel, "Channel", required=True)):
+        try:
+            guild = ctx.guild
+            if database.lookUpGuildReport(guild.id):
+                database.updateGuildReport(guild.id, channel.id)
+                await ctx.respond("Updated the report channel for this server", ephemeral=True)
+            else:
+                database.setGuildReport(guild.id, channel.id)
+                await ctx.respond("Added a report channel for this server", ephemeral=True)
+        except discord.ext.commands.errors.MissingAnyRole:
+            await ctx.respond("Something went wrong, are you a mod or admin?")
+
+    @slash_command(guild_ids=[365879579887534080], default_permission=False)
+    @commands.has_any_role("ADMIN", "MOD")
+    async def removereportchannel(self, ctx: discord.ApplicationContext):
         guild = ctx.guild
         if database.lookUpGuildReport(guild.id):
             database.removeGuildReport(guild.id)
-            await ctx.send("Removed the report channel for this server")
+            await ctx.respond("Removed the report channel for this server", ephemeral=True)
 
-    @commands.command(pass_context=True)
-    @has_permissions(administrator=True)
-    async def toggleRoleMemory(self, ctx):
+    @slash_command(guild_ids=[365879579887534080], default_permission=False)
+    @commands.has_any_role("ADMIN", "MOD")
+    async def togglerolememory(self, ctx: discord.ApplicationContext):
+        status = database.checkRoleMemory(ctx.guild.id)
+        msg = ""
+        if status == 1:
+            msg = "Role memory has been turned off for this server"
+        if status == 0:
+            msg = "Role memory has been turned on for this server"
         database.toggleRoleMemory(ctx.guild.id)
+        await ctx.respond(msg, ephemeral=True)
+    
+    @slash_command(guild_ids=[365879579887534080], default_permission=False)
+    @commands.has_any_role("ADMIN", "MOD")
+    async def checkrolememory(self, ctx: discord.ApplicationContext):
+        status = database.checkRoleMemory(ctx.guild.id)
+        msg = ""
+        if status == 1:
+            msg = "Role memory is turned on on this server"
+        else:
+            msg = "Role memory is turned off on this server"
+        await ctx.respond(msg, ephemeral=True)
 def setup(bot):
     bot.add_cog(Admin(bot))
