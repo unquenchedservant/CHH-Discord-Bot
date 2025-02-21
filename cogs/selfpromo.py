01b90e77ac9fa28a266225622c57363023e0b017
Jon Thorne <jon@jonthorne.com>
2025-02-21 15:00:19 -0500

    Add Context menu option

--- cogs/selfpromo.py
+++ cogs/selfpromo.py
@@ -2,7 +2,7 @@ from ctypes import util
 
 import discord
 import utilities
-from discord.commands import Option, slash_command
+from discord.commands import Option, slash_command, message_command
 from discord.ext import commands
 from utilities import database
 
@@ -14,7 +14,21 @@ RULE_CHANNEL = utilities.get_rules_id()
 class SelfPromo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    @message_command(name="Mark Self-Promo")
+    async def selfpromo(self,ctx, message: discord.Message):
+        user = message.author
+        if user:
+            msg = "Woah there, <@" + str(user.id) + ">,"
+        else:
+            msg = "Woah there,"
+        msg = msg + " it looks like you're sharing self-promotion outside of <#" + str(SELF_PROMO_CHANNEL) + ">!\n\n"
+        msg = msg + "If you don't have access to that channel, please stick around and get to know us a bit. Shortly after you join you will gain access. \n\n"
+        msg = msg + "In the meantime, check out <#" + str(ROLE_MENU_CHANNEL) + "> and assign yourself the Artist/Producer tag to unlock some extra channels. Please take a minute to check out our <#" + str(RULE_CHANNEL) + ">\n\n"
+        msg = msg + "If we don't know who you are, we likely won't care about your music."
+        embed = discord.Embed(title="Please don't self promo", description=msg)
+        #embed.set_image(url="https://i.imgur.com/MQMdhiE.jpeg")
+        await ctx.send(embed=embed)
+        await ctx.respond("Thanks, we let the user know about our self promotion rule!", ephemeral=True)
     @slash_command(
         guild_ids=GUILD_ID,
         default_permissions=True,
