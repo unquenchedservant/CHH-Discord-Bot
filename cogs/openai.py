86a2f7dda684191c318866d99d76e2e76543867a
Jonathan Thorne <jon@jonthorne.com>
2025-09-06 15:17:55 -0400

    Added user prompt to output of /ask

--- cogs/openai.py
+++ cogs/openai.py
@@ -7,29 +7,28 @@ class OpenAI(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def get_answer(self, ctx, prompt):
+    def get_name(self, ctx):
         if not ctx.user.global_name == None:
             uname = ctx.user.global_name
         elif not ctx.user.nick == None:
             uname = ctx.user.nick
         else:
             uname = ctx.user.name
-        return openai.generate_answer(prompt, uname=uname)
+        return uname
     
     @slash_command(
-        
         default_permissions=True,
         description="Ask CHHBot a question (Beta)",
     )
     async def ask(self, ctx: discord.ApplicationContext, prompt: Option(str, 'Prompt for CHHBot', required=True), hide: Option(bool, "Do you want this to show only for you?", required=False, default=False)):
-        print(f"THIS IS THE HIDE VALUE {hide}")
         await ctx.defer(ephemeral=hide)
+        uname = self.get_name(ctx)
         if not hide:
             async with ctx.channel.typing():
-                response = self.get_answer(ctx, prompt)
+                response = openai.generate_answer(prompt, uname=uname)
+                response = f"{uname} asked: \n{prompt}\n---\n" + response 
         else:
-            response = self.get_answer(ctx, prompt)
-        print(f"THIS IS THE HIDE2 VALUE {hide}")
+            response = openai.generate_answer(prompt, uname=uname)
         await ctx.respond(response, ephemeral=hide)
 def setup(bot):
     bot.add_cog(OpenAI(bot))
